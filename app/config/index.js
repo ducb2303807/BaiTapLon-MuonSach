1fde4ee36add103849e114915ef89642da7da200
ducb2303807 <ducb2303807@student.ctu.edu.vn>
2025-10-27 23:07:06 +0700

    first commit

---
+++ app/config/index.js
@@ -0,0 +1,9 @@
+const config = {
+    app: {
+        port: process.env.APP_PORT || 3000,
+    },
+    db: {
+        uri: process.env.DB_URI || "mongodb://localhost:27017/BorrowBooks",
+    }
+}
+module.exports = config;
